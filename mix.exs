6c7547e62956ef9ef8ff844547721518e0ff9320
BennyHallett <benny.hallett@gmail.com>
2014-08-08 08:42:15 +1000

    Update to 0.15.0

--- mix.exs
+++ mix.exs
@@ -3,29 +3,17 @@ defmodule Bennyhallett.Mixfile do
 
   def project do
     [app: :bennyhallett,
-     version: "0.0.1",
-     elixir: "~> 0.14.1",
+     version: "0.1.0",
+     elixir: "~> 0.15.0",
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:obelisk]]
   end
 
-  # Dependencies can be hex.pm packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    #[{ :obelisk, github: "bennyhallett/obelisk" }]
-    [{ :obelisk, git: "file:///home/benny/projects/obelisk" }]
+    [{ :obelisk, "0.4.0" },
+     { :yamerl, github: "yakaz/yamerl" }]
   end
 end
